385165eab801816a7ad756c2f9ef392352103d50
HA MANH THAI <hamanhthai40@gmail.com>
2024-05-23 18:18:01 +0700

    thucHanh4

--- Lesson03/cookie-demo.php
+++ Lesson03/cookie-demo.php
@@ -30,12 +30,17 @@
     <form action="" method="POST">
         <div class="mb-3">
             <label for="email" class="form-label">Email address</label>
+
             <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+
         </div>
         <div class="mb-3">
             <label for="pass" class="form-label">Password</label>
+
             <input type="password" class="form-control" id="pass" name="pass">
+            
         </div>
         <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="remember" name="remember">
